e385f2f106432d1fefb8c87ed757609e101d7cff
lflandoli <flandoli.lucas@gmail.com>
2025-08-26 11:12:40 -0300

    Add script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function addTask() {
+  const taskInput = document.getElementById("task-input");
+  const projectInput = document.getElementById("project-input");
+  const todoList = document.querySelector("#todo ul");
+
+  if (taskInput.value.trim() === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = `[${projectInput.value}] ${taskInput.value}`;
+  li.draggable = true;
+
+  li.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text/plain", li.outerHTML);
+    li.remove();
+  });
+
+  todoList.appendChild(li);
+  taskInput.value = "";
+}
+
+// Permitir arrastar entre colunas
+document.querySelectorAll(".column ul").forEach((ul) => {
+  ul.addEventListener("dragover", (e) => e.preventDefault());
+  ul.addEventListener("drop", (e) => {
+    e.preventDefault();
+    ul.insertAdjacentHTML("beforeend", e.dataTransfer.getData("text/plain"));
+    ul.lastChild.draggable = true;
+  });
+});
 
